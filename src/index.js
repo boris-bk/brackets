edf6cb506f3eb5c58e55193aee0c108b23b35051
Boris <boris.bortnitskii@yandex.by>
2022-08-11 17:26:06 +0400

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) return false;
+    let re = '';
+    for (let o = 0; o < str.length*str.length*2; o++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            for (let j = 0; j < str.length; j++) {
+                re = bracketsConfig[i][0] + bracketsConfig[i][1]
+                str = str.replace(re, '')
+            }
+        }
+    }
+    if (str === '') {
+        return true
+    }
+    else {
+        return false
+    }
 }
